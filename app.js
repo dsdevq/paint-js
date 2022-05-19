d05c0a8b207580ed9bb99f69fa5a89bfbf671417
Denis <payar3282@gmail.com>
2022-05-19 10:09:13 +0300

    last commit

--- app.js
+++ app.js
@@ -1,33 +1,22 @@
 const canvas = document.querySelector('#jsCanvas')
 const ctx = canvas.getContext('2d')
-const colors = document.getElementsByClassName('jsColor')
-const range = document.getElementById('jsRange')
-const mode = document.getElementById('jsMode')
-const saveBtn = document.getElementById('jsSave')
+const colors = document.querySelectorAll('.jsColor')
+const range = document.querySelector('#jsRange')
+const mode = document.querySelector('#jsMode')
+const saveBtn = document.querySelector('#jsSave')
 const infocolor = document.querySelector('.controls__infocolor')
 const inforange = document.querySelector('.controls__inforange')
-
-console.log(infocolor.textContent, inforange.textContent);
-
-const logger = (event) => {
-    event.target.textContent = 'changed'
-}
-
-infocolor.addEventListener('click', logger)
-inforange.addEventListener('click', logger)
-
 
 let painting = false
 let filling = false
 
 const CANVAS_SIZE = 700
 
-canvas.height = CANVAS_SIZE;
-canvas.width = CANVAS_SIZE;
+canvas.height = canvas.width = CANVAS_SIZE;
+
 
 ctx.fillStyle = 'white'
 ctx.fillRect(0,0, CANVAS_SIZE, CANVAS_SIZE)
-
 ctx.lineWidth = 2.5;
 ctx.strokeStyle = 'black';
 
@@ -54,42 +43,32 @@ function onMouseMove(event) {
     }
 }
 
-function onMouseDown(event) {
-    console.log('onMouseDown', event);
+function onMouseDown() {
     painting = true
 }
 
 function handleColorClick(event) {
     const color = event.target.style.backgroundColor
-    ctx.strokeStyle = color
-    ctx.fillStyle = color
-    infocolor.style.backgroundColor = color
-
+    ctx.strokeStyle = ctx.fillStyle = infocolor.style.backgroundColor = color 
 }
 
 function handleRangeChange(event) {
     const rangeValue = event.target.value
-    ctx.lineWidth = rangeValue
-    inforange.textContent = rangeValue
+    ctx.lineWidth = inforange.textContent = rangeValue
 }
 
 function handleModeClick() {
     if (filling) {
-        filling = false
-        // filling = !filling
+        filling = !filling
         mode.innerText = 'Fill'
     } else {
-        filling = true
-        // filling = !filling
+        filling = !filling
         mode.innerText = 'Draw'
     }
 }
 
 function handleCanvasClick() {
-    console.log('handleCanvasClick');
-    if (filling) {
-        ctx.fillRect(0,0,CANVAS_SIZE, CANVAS_SIZE)
-    }
+    if (filling) ctx.fillRect(0,0,CANVAS_SIZE, CANVAS_SIZE)
 }
 
 
